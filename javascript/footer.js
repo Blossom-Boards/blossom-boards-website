61aa524eef3287633712d3ecad9224f41e90346d
Simon Dao <SimonNDao12@gmail.com>
2022-12-30 19:19:54 -0800

    headers and footers are not automatically generated

---
+++ javascript/footer.js
@@ -0,0 +1,12 @@
+function footer() {
+  let footerContainer = document.createElement('footer')
+  
+  footerContainer.innerHTML = `
+    
+  `
+
+  let body = document.querySelector('body')
+  body.appendChild(footerContainer)
+}
+
+footer()
---
+++ javascript/header.js
@@ -0,0 +1,23 @@
+function header() {
+  let headerContainer = document.createElement('div')
+  
+  headerContainer.innerHTML = `
+  
+  <a class="hover-text" href="main.html"><h1 id="name">Blossom Boards</h1></a>
+  <img id="icon" src="../images/icon.png"></img>
+  <nav>
+      <a class="hover-text" href="main.html"><h1 class="nav-text">Home</h1></a>
+      <a class="hover-text" href="prebuilts.html"><h1 class="nav-text">SHOP PRE-BUILTS</h1></a>
+      <a class="hover-text" href="custombuilds.html"><h1 class="nav-text">CUSTOM BUILDS</h1></a>
+      <a class="hover-text" href="services.html"><h1 class="nav-text">SERVICES</h1></a>
+      <a class="hover-text" href="faqs.html"><h1 class="nav-text">FAQs</h1></a>
+      <a class="hover-text" href="about.html"><h1 class="nav-text">About Us</h1></a>
+  </nav>
+  <hr>
+  `
+
+  let body = document.querySelector('body')
+  body.insertBefore(headerContainer, body.firstChild)
+}
+
+header()
