11e6a8375e82f79a1ccd10a8efd72321063418a8
Simon Dao <SimonNDao12@gmail.com>
2023-01-06 18:17:41 -0800

    changed main.html to index.html

--- javascript/header.js
+++ javascript/header.js
@@ -7,7 +7,7 @@ function header() {
   <a class="hover-text" href="main.html"><h1 id="name">Blossom Boards</h1></a>
   <img id="icon" src="../images/icon.png"></img>
   <nav>
-      <a class="hover-text" href="main.html"><h1 class="nav-text">HOME</h1></a>
+      <a class="hover-text" href="index.html"><h1 class="nav-text">HOME</h1></a>
       <a class="hover-text" href="prebuilts.html"><h1 class="nav-text">SHOP PRE-BUILTS</h1></a>
       <a class="hover-text" href="custombuilds.html"><h1 class="nav-text">CUSTOM BUILDS</h1></a>
       <a class="hover-text" href="services.html"><h1 class="nav-text">SERVICES</h1></a>
